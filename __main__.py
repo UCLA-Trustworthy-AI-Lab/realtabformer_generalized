499837b3663d19fad4a33c84d473843566a4cb92
ThomasK1018 <thomask1018999@gmail.com>
2024-12-04 12:03:48 -0500

    Update
    Parent table synthesis is generalized.
    
    Need to figure out an encoder-decoder structure as Llama cannot be used as the decoder? But Llama is supposingly a decoder-only model?

--- __main__.py
+++
@@ -1,9 +0,0 @@
-"""Entry point when invoked with python -m realtabformer."""  # pragma: no cover
-
-if __name__ == "__main__":  # pragma: no cover
-    import sys
-
-    import realtabformer
-
-    if "--version" in sys.argv:
-        print(f"REaLTabFormer version: {realtabformer.__version__}")
